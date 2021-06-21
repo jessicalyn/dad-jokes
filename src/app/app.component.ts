1a5ad15bba46d60ea7ad82251d3975a8c8ee99fd
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2021-06-20 21:19:53 -0600

    Make button retrieve jokes and display grid of all jokes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,22 @@ import { JokeResponse } from './joke';
 export class AppComponent implements OnInit {
   title: string = 'Dad Jokes';
   currentJoke: string = '';
+  jokes: Array<string> = [];
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.newJokeAPI()
+  }
+
+  newJokeAPI() {
     this.apiService.getNewJoke()
-      .subscribe((jokeResponse: JokeResponse) => {
-        this.currentJoke = jokeResponse.joke
-      })
+    .subscribe((jokeResponse: JokeResponse) => {
+      this.jokes.unshift(jokeResponse.joke)
+    })
+  }
+
+  jokeButton() {
+    this.newJokeAPI()
   }
 }
